5561d16b662fd131c9fc98ee416a5b551ef400f9
Karolina Kulinska <karolina.kulinska89@gmail.com>
2021-07-02 10:11:00 +0200

    added class component and using set state to change title

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -5,20 +5,55 @@ import Form from '../Form/Form'
 
 
 
-function UserList() {
-    return (
-        <div>
+// function UserList() {
+//     return (
+//         <div>
+//             <div>
+//                 <h1>Simple List</h1>
+//             </div>
+//             <Form />
+//             <ul>
+//                 {users.map(({ name, surname, average, attendance = '0%' }, index) => (
+//                     <StudentsListItem index={index} name={name} surname={surname} attendance={attendance} average={average} />
+//                 ))}
+//             </ul>
+//         </div>
+//     )
+// }
+
+
+class UserList extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isStudentList: true,
+        }
+    }
+
+    handleChangeTitleList = () => {
+        this.setState((prevState) => ({
+            isStudentList: !prevState.isStudentList,
+
+        }))
+    }
+
+    render() {
+        return (
             <div>
-                <h1>Simple List</h1>
+                <div>
+                    <h1>{this.state.isStudentList ? 'Student`s list' : 'User`s list'}</h1>
+                    <button onClick={this.handleChangeTitleList} >Change List</button>
+                </div>
+                <Form />
+                <ul>
+                    {users.map(({ name, surname, average, attendance = '0%' }, index) => (
+                        <StudentsListItem index={index} name={name} surname={surname} attendance={attendance} average={average} />
+                    ))}
+                </ul>
             </div>
-            <Form />
-            <ul>
-                {users.map(({ name, surname, average, attendance = '0%' }, index) => (
-                    <StudentsListItem index={index} name={name} surname={surname} attendance={attendance} average={average} />
-                ))}
-            </ul>
-        </div>
-    )
+        )
+    }
 }
 
 
